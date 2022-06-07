fe18374778dfed4a80a91c54bfaf2cc4ff94401a
Céline LY <lycelinepro99@gmail.com>
2022-06-07 21:48:48 +0200

    add map

---
+++ Room_Builder_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="Room_Builder_32x32" tilewidth="32" tileheight="32" tilecount="8284" columns="76">
+ <image source="../../../Downloads/Modern_Interiors/1_Interiors/32x32/Room_Builder_32x32.png" width="2432" height="3488"/>
+</tileset>
