a2ce87a82f0ea2ce697fe8c984e6a62c29c256c4
Céline LY <lycelinepro99@gmail.com>
2022-06-08 20:01:41 +0200

    [FIX] Change condition

--- src/main.ts
+++ src/main.ts
@@ -8,29 +8,50 @@ WA.onInit()
     console.log("Scripting API ready");
     console.log("Player tags: ", WA.player.tags);
     // Attribution du rôle par défaut
-    WA.player.state.roleBuyer = "buyer";
+    WA.player.state.role = "buyer";
     console.log(WA.player.state.role);
+    // Variable globale pour check si role Seller est déjà attribué
+    WA.state.saveVariable("alreadyGiveRoleSeller", false);
 
+    WA.state
+      .onVariableChange("alreadyGiveRoleSeller")
+      .subscribe((alreadyGiveRoleSeller) => {
+        console.log(
+          'Variable "config" changed. New value: ',
+          alreadyGiveRoleSeller
+        );
+      });
     // BeSellerZone
     WA.room.onEnterLayer("beSellerZone").subscribe(() => {
       console.log("seller zone");
-      if (!(WA.player.state.roleSeller == "seller")) {
-        if ((WA.player.state.role == "seller") === null || undefined) {
-          console.log("There is already a seller, you cant enter");
-          WA.player.moveTo(250, 250, 10);
-        } else {
-          const triggerMessage = WA.ui.displayActionMessage({
-            message: "Wanna be a seller ? press 'space' to confirm",
-            callback: () => {
-              WA.player.state.roleSeller = "seller";
-              console.log(WA.player.state.role);
-              WA.chat.sendChatMessage("confirmed", "You're a seller now");
-            },
-          });
-          setTimeout(() => {
-            triggerMessage.remove();
-          }, 10000);
-        }
+
+      if (WA.state.loadVariable("alreadyGiveRoleSeller") === false) {
+        const triggerMessage = WA.ui.displayActionMessage({
+          message: "Wanna be a seller ? press 'space' to confirm",
+          callback: () => {
+            WA.state.config = {
+              alreadyGiveRoleSeller: true,
+            };
+            WA.state.saveVariable("alreadyGiveRoleSeller", true);
+            //WA.player.state.role = "seller";
+            console.log(WA.player.state.role);
+            //console.log("alreadyGiveRoleSeller", config);
+            WA.chat.sendChatMessage("confirmed", "You're a seller now");
+          },
+        });
+        setTimeout(() => {
+          triggerMessage.remove();
+        }, 6000);
+      } else if (
+        WA.state.loadVariable("alreadyGiveRoleSeller") === true &&
+        (WA.player.state.role = "buyer")
+      ) {
+        console.log("There is already a seller, you cant enter");
+        console.log(
+          "alreadyGiveRoleSeller",
+          WA.state.loadVariable("alreadyGiveRoleSeller")
+        );
+        WA.player.moveTo(250, 250, 10);
       }
     });
     WA.room.onLeaveLayer("beSellerZone").subscribe(() => {
@@ -49,7 +70,7 @@ WA.onInit()
       setTimeout(() => {
         // later
         triggerMessage.remove();
-      }, 10000);
+      }, 6000);
       // Object Sell Appear
       const objectSell = await WA.ui.website.open({
         url: "https://wikipedia.org/",
